216a8729bf8f6ae3912d42cf91b9b2d6e5a4af3d
root <root@DESKTOP-2KLJLBF.localdomain>
2020-03-06 13:59:25 -0600

    Equipment populates onto DOM from Javascript.

---
+++ scripts/equipment/Equipment.js
@@ -0,0 +1,12 @@
+const Equipment = (equipment) => {
+    return `
+        <div class="equipment">
+            <div class="equipment__model">${equipment.model}</div>
+            <div class="equipment__serial__number">Serial Number: ${equipment.serialNumber}</div>
+            <div class="equipment__functional">Functianal: ${equipment.isFunctional}</div>
+            <div class="equipment__under__warranty">Under Warranty: ${equipment.isUnderWarranty}</div>
+        </div>
+        `
+    }
+
+export default Equipment
--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,10 @@
 import EmployeeList from "./employee/EmployeeList.js";
 import ProductList from "./product/ProductList.js";
+import EquipmentList from "./equipment/EquipmentList.js";
+
+
+EquipmentList()
+
+EmployeeList()
 
 ProductList()
-EmployeeList()
